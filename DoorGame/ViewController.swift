b10be32bd6e5659d2a65038684568bf8d0a71a1b
Maral <marallsharipkyzy@gmail.com>
2025-11-15 18:20:36 +0500

    Initial commit

---
+++ DoorGame/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  DoorGame
+//
+//  Created by Maral on 15.11.2025.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
