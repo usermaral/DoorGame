8f1dab6a251194c90a70d04bb3b9bd3b16f68127
Maral <marallsharipkyzy@gmail.com>
2025-11-16 21:18:58 +0500

    Added resourses

---
+++ DoorGame/Coordinator/Coordinator.swift
@@ -0,0 +1,8 @@
+//
+//  Coordinator.swift
+//  DoorGame
+//
+//  Created by Maral on 16.11.2025.
+//
+
+import Foundation
---
+++ DoorGame/UI/GameUI.swift
@@ -0,0 +1,8 @@
+//
+//  GameUI.swift
+//  DoorGame
+//
+//  Created by Maral on 16.11.2025.
+//
+
+import Foundation
